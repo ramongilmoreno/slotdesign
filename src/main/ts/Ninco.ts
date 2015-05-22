7dd54a3d15b2534e99e6a31ea86ae7b0ed1b7b8d
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2015-05-22 20:04:07 +0200

    Some actual Ninco pieces defined with real geometry. Pieces include now the SVG path definition and bounding box. Index showcases the different pieces.

--- src/main/ts/Ninco.ts
+++ src/main/ts/Ninco.ts
@@ -22,8 +22,27 @@ module Ninco {
     // http://www.ninco.com/product/6/0/0/1/2/2-X-STRAIGHT-20-CM.htm
     Parts.push(straight("10103", "STRAIGHT 20 CM", "Straight section of 200 millimeters", 200));
     
+    // http://www.armchairracer.com.au/track-systems/ninco/ninco-track-and-border-table
+    // http://slotadictos.mforos.com/21954/7223571-medidas-pista-ninco/#70156625
+    // Curva          Borde interior    Carril interior   Carril exterior   Borde exterior
+    // Interior           6.15              10.65             19.65             24.15
+    // Standard          24.15              28.65             37.65             42.15
+    // Exterior          42.15              46.65             55.65             60.15
+    // Super-exterior    60.15              64.65             73.65             78.15
     
+    // http://www.ninco.com/product/9/0/0/1/2/2-X-INNER-CURVE.htm
+    // Interior           6.15              10.65             19.65             24.15
+    var baseRadius: number = 61.5;
+    Parts.push(curve("10106", "INNER CURVE", "Inner curve 45 degrees", baseRadius + (0 * WIDTH), Math.PI / 4));
     // http://www.ninco.com/product/8/0/0/1/2/2-X-STANDARD-CURVE.htm
     // Standard          24.15              28.65             37.65             42.15
-    Parts.push(curve("10105", "STANDARD CURVE - RIGHT", "Regular curve 45 degrees", 2415, 45));
+    Parts.push(curve("10105", "STANDARD CURVE", "Regular curve 45 degrees", baseRadius + (1 * WIDTH), Math.PI / 4));
+    // http://www.ninco.com/product/10/0/0/1/2/2-X-OUTER-CURVE.htm
+    // Exterior          42.15              46.65             55.65             60.15
+    Parts.push(curve("10107", "OUTER CURVE", "Outer curve 22.5 degrees", baseRadius + (2 * WIDTH), Math.PI / 8));
+    // http://www.ninco.com/product/11/0/0/1/2/2-X-GRAND-CURVE.htm
+    // Super-exterior    60.15              64.65             73.65             78.15
+    Parts.push(curve("10108", "GRAND CURVE", "Next outer (grand) curve 22.5 degrees", baseRadius + (3 * WIDTH), Math.PI / 8));
+    // http://www.ninco.com/product/17/0/0/1/2/2-X-ULTRA-EXTERIOR-CURVE.htm
+    Parts.push(curve("10115", "ULTRA EXTERIOR CURVE", "Outmost curve 11.05 degrees", baseRadius + (4 * WIDTH), Math.PI / 16));
 }
--- src/main/ts/Pieces.ts
+++ src/main/ts/Pieces.ts
@@ -14,6 +14,21 @@ module Pieces {
         set y(y: number) { this._y = y; }
     }
 
+    export interface IBox {
+        topLeft: ICoordinates;
+        bottomRight: ICoordinates;
+    }
+
+    export class DefaultBox implements IBox {
+        private _topLeft: ICoordinates;
+        private _bottomRight: ICoordinates;
+        constructor(topLeft: ICoordinates = new DefaultCoordinates(), bottomRight: ICoordinates = new DefaultCoordinates()) { this._topLeft = topLeft; this._bottomRight = bottomRight; }
+        get topLeft(): ICoordinates { return this._topLeft; }
+        set topLeft(topLeft: ICoordinates) { this._topLeft = topLeft; }
+        get bottomRight(): ICoordinates { return this._bottomRight; }
+        set bottomRight(bottomRight: ICoordinates) { this._bottomRight = bottomRight; }
+    }
+
     export interface IIdentified {
         id: string;
         name: string;
@@ -41,6 +56,8 @@ module Pieces {
         offset: ICoordinates;
         rotation: number;
         manufacturer: string;
+        svgPath: string;
+        box: IBox;
     }
 
     export class AbstractTrackPiece extends DefaultIdentified {
@@ -81,6 +98,19 @@ module Pieces {
             return 0;
         }
         set rotation(rotation: number) { console.log("Unsupported"); }
+        get svgPath(): string {
+            var w: number = this.width;
+            var l: number = this.length;
+            var w2: number = Math.round(w / 2.0);
+            return "l -" + w2 + ",0 l 0,-" + l + " l " + w + ",0 l 0," + l + " l -" + w2 + ",0";
+        }
+        set svgPath(svgPath: string) { console.log("Unsupported"); }
+
+        get box(): IBox {
+            var w2: number = Math.round(this.width / 2.0);
+            return new DefaultBox(new DefaultCoordinates(-w2, -this.length), new DefaultCoordinates(w2, 0));
+        }
+        set box(box: IBox) { console.log("Unsupported"); }
     }
 
     export class DefaultCurve extends AbstractTrackPiece implements ITrackPiece {
@@ -102,6 +132,89 @@ module Pieces {
             return this._arc;
         }
         set rotation(rotation: number) { console.log("Unsupported"); }
+
+        get svgPath(): string {
+            var arc = this._arc % (2 * Math.PI);
+            if (arc < 0) {
+                arc += (2 * Math.PI);
+            }
+
+            var w: number = this.width;
+            var w2: number = Math.round(w / 2.0);
+            var outerRadius = this._innerRadius + w;
+            var outerSin: number = Math.sin(arc) * outerRadius;
+            var outerCos: number = Math.cos(arc) * outerRadius;
+            var innerSin: number = Math.sin(arc) * this._innerRadius;
+            var innerCos: number = Math.cos(arc) * this._innerRadius;
+
+            // inner vertex
+            // innerCos
+            // innerSin
+
+            // outer vertex
+            // outerCos
+            // outerSin
+            
+            // a <x radius>,<y radius> 0 0,1 <end x>,<end y>
+            var box = this.box;
+            return "l " + (-w2) + ",0" +
+                " a " + this._innerRadius + "," + this._innerRadius + " 0 0,0 " + (innerCos - this._innerRadius) + "," + (-innerSin) +
+                " l " + (outerCos - innerCos) + "," + (-outerSin + innerSin) +
+                " a " + outerRadius + "," + outerRadius + " 0 0,1 " + (outerRadius - outerCos) + "," + outerSin +
+                " l " + (-w2) + ", 0"
+                // reveal the bbox for debugging purposes
+                // + " m " + box.topLeft.x + "," + box.topLeft.y + " l " + (box.bottomRight.x - box.topLeft.x) + "," + (box.bottomRight.y - box.topLeft.y)
+                ;
+        }
+        set svgPath(svgPath: string) { console.log("Unsupported"); }
+        get box(): IBox {
+            var arc = this._arc % (2 * Math.PI);
+            if (arc < 0) {
+                arc += (2 * Math.PI);
+            }
+
+            var w: number = this.width;
+            var w2: number = Math.round(w / 2.0);
+            var outerRadius = this._innerRadius + w;
+            var outerSin: number = Math.sin(arc) * outerRadius;
+            var outerCos: number = Math.cos(arc) * outerRadius;
+            var innerSin: number = Math.sin(arc) * this._innerRadius;
+            var innerCos: number = Math.cos(arc) * this._innerRadius;
+
+            // inner vertex
+            // innerCos
+            // innerSin
+
+            // outer vertex
+            // outerCos
+            // outerSin
+
+            var topLeft: ICoordinates = new DefaultCoordinates();
+            var bottomRight: ICoordinates = new DefaultCoordinates();
+            if (arc <= (Math.PI / 2)) {
+                topLeft.x = innerCos - this._innerRadius - w2 ;
+                topLeft.y = -outerSin;
+                bottomRight.x = w2;
+                bottomRight.y = 0;
+            } else if (arc <= (Math.PI)) {
+                topLeft.x = -w2 - this._innerRadius + outerCos;
+                topLeft.y = -outerRadius;
+                bottomRight.x = w2
+                bottomRight.y = 0;
+            } else if (arc <= (3 * Math.PI / 2)) {
+                topLeft.x = -w2 - this._innerRadius - outerRadius;
+                topLeft.y = -outerRadius;
+                bottomRight.x = w2;
+                bottomRight.y = -outerSin;
+            } else if (arc <= (Math.PI * 2)) {
+                topLeft.x = -w2 - this._innerRadius - outerRadius;
+                topLeft.y = -outerRadius;
+                bottomRight.x = w2;
+                bottomRight.y = outerRadius;
+            }
+            return new DefaultBox(topLeft, bottomRight);
+        }
+
     }
 
     export interface ITrackSection {
@@ -118,8 +231,8 @@ module Pieces {
         }
         get rotate(): boolean { return this._rotate; }
         set rotate(rotate: boolean) { this._rotate = rotate; }
-        get piece (): ITrackPiece { return this._piece; }
-        set piece (piece: ITrackPiece) { this._piece = piece; }
+        get piece(): ITrackPiece { return this._piece; }
+        set piece(piece: ITrackPiece) { this._piece = piece; }
     }
 
     export interface ITrack extends IIdentified {
