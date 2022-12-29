463c58ceb547aa86db052d192f4df86439a7b2af
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2022-12-29 14:59:46 +0100

    --allow-root to try running build from a Docker container

--- run.sh
+++ run.sh
@@ -7,7 +7,12 @@ cp -RT src/main/webapp build
 rm -rf build/WEB-INF
 
 npx tsc
-npx bower install
+
+#
+# https://stackoverflow.com/questions/25672924/run-bower-from-root-user-its-possible-how
+# Requires this "--allow-root" flag to let Docker container run this as root
+#
+npx bower install --allow-root
 
 cp -R bower_components build/bower_components
 
